2db94143c6c8916bdb5a146a9fdda62324d74590
whynot <anil@panix.com>
2025-05-28 11:46:30 -0400

    Spring Boot 2.5

--- code/sbdemo/src/main/java/ttl/larku/SbdemoApplication.java
+++ code/sbdemo/src/main/java/ttl/larku/SbdemoApplication.java
@@ -1,11 +1,11 @@
 package ttl.larku;
 
 import java.util.List;
+import javax.sound.midi.Track;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import ttl.larku.domain.Student;
 import ttl.larku.service.StudentService;
@@ -38,4 +38,20 @@ class MyRunner implements CommandLineRunner {
 	}
 }
 
+
+//@Component
+//class YourRunner implements CommandLineRunner {
+//
+//	@Autowired
+//	private TrackService trackService;
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		System.out.println("Hello from MyRunner");
+//
+//		List<Track> tracks = trackService.getAllTracks();
+//
+//		System.out.println(tracks);
+//	}
+//}
 
